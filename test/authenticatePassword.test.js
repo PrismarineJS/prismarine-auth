dcdc8ac13d9a103620af921105c8034316e89774
Jordan Jones <jordpjones@gmail.com>
2021-06-27 13:38:28 -0700

    Not sure how we want to do tests

--- test/authenticatePassword.test.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env jest */
-
-const { authenticatePassword } = require('../src/MicrosoftAuthentication')
-
-describe('authenticatePassword', () => {
-  test('throws error', () => {
-    expect(() => authenticatePassword.toThrow('Not implemented'))
-  })
-})
