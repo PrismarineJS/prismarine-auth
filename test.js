b0f7d931e8cc53d3e5151ff40d354678c3572c0b
Jordan Jones <jordpjones@gmail.com>
2021-06-27 03:03:37 -0700

    User Test

---
+++ test.js
@@ -0,0 +1,10 @@
+const test = require('./');
+
+const user = { username: 'not.a.valid.email@cuz.yeah'};
+
+const init = async () => {  
+    const auth = await test.Authenticate(user)
+    console.log(auth)
+}
+
+init();
