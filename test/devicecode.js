1d85912edb36ded55877c65ff4fdce2ca77f1dd4
Jordan Jones <jordpjones@gmail.com>
2021-06-29 21:26:17 -0700

    add some tests

---
+++ test/devicecode.js
@@ -0,0 +1,23 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const { expect } = chai
+
+const XboxLiveAuth = require('../')
+
+describe('device code authentication', () => {
+  it('should fail if not given any options', async () => {
+    await expect(XboxLiveAuth.authenticate({ })).to.eventually.be.rejectedWith('options.username is a required identifier')
+  })
+  it('should fail if not given the right options', async () => {
+    await expect(XboxLiveAuth.authenticate(false)).to.eventually.be.rejected
+  })
+  it('should give us a token', (done) => {
+    const onMsaCode = (code) => {
+      if (!code) done(Error('missing user code'))
+      if (code.userCode) done()
+    }
+    XboxLiveAuth.authenticate({ username: 'emailIdentifier@test.prismarine', onMsaCode })
+  })
+})
---
+++ test/password.js
@@ -0,0 +1,13 @@
+/* eslint-env mocha */
+const chai = require('chai')
+const chaiAsPromised = require('chai-as-promised')
+chai.use(chaiAsPromised)
+const { expect } = chai
+
+const XboxLiveAuth = require('../')
+
+describe('password authentication', () => {
+  it('should fail if not given a valid password', async () => {
+    await expect(XboxLiveAuth.authenticate({ username: 'this.is.not@valid.email.lol', password: 'sdfasdgf' })).to.eventually.be.rejectedWith('Invalid credentials')
+  })
+})
