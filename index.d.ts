6501490fa188048d8d4272d05df76dd6333b9045
extremeheat <extreme@protonmail.ch>
2021-08-10 16:43:15 -0400

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,38 @@
 /// <reference types="node" />
 
 declare module 'prismarine-auth' {
-    export class MicrosoftAuthFlow {
-        constructor(username: string, cache: string, options?: MicrosoftAuthFlowOptions, codeCallback?: Function)
-        initTokenCaches?: (username: string, cache: string) => void
-        resetTokenCaches?: (cache: string) => boolean
-        getMsaToken?: () => string
-        getXboxToken?: () => string
-        getMinecraftJavaToken?: (MinecraftJavaToken: object) => string
-        getMinecraftBedrockToken?: (publicKey: string) => string
-    }
-    export interface MinecraftJavaToken {
-        fetchEntitlements: boolean = false
-        fetchProfile: boolean = false
-    }
+  export class AuthFlow {
+    constructor(username: string, cache: string, options?: MicrosoftAuthFlowOptions, codeCallback?: Function)
+    initTokenCaches: (username: string, cache: string) => void
+    resetTokenCaches: (cache: string) => boolean
+    // Returns a Microsoft Oauth access token -- https://docs.microsoft.com/en-us/azure/active-directory/develop/access-tokens
+    getMsaToken: () => string
+    // Returns an XSTS token -- https://docs.microsoft.com/en-us/gaming/xbox-live/api-ref/xbox-live-rest/additional/edsauthorization
+    getXboxToken: () => string
+    // Returns a Minecraft Java Edition auth token
+    getMinecraftJavaToken: (MinecraftJavaToken: object) => { token: string, entitlements: object, profile: object }
+    // Returns a Minecraft Bedrock Edition auth token. Public key parameter must be a KeyLike object.
+    getMinecraftBedrockToken: (publicKey) => string
+  }
+  export interface MinecraftJavaToken {
+    fetchEntitlements: boolean
+    fetchProfile: boolean
+  }
 
-    export interface MicrosoftAuthFlowOptions {
-        relyingParty?: RelyingParty
-        authTitle?: Titles
-        password?: String
-    }
+  export interface MicrosoftAuthFlowOptions {
+    relyingParty?: RelyingParty
+    authTitle?: Titles
+    password?: String
+  }
 
-    export enum Titles {
-        MinecraftNintendoSwitch = '00000000441cc96b',
-        MinecraftJava = '00000000402b5328'
-    }
+  export enum Titles {
+    MinecraftNintendoSwitch = '00000000441cc96b',
+    MinecraftJava = '00000000402b5328'
+  }
 
-    export enum RelyingParty {
-        PCXSTSRelyingParty = 'rp://api.minecraftservices.com/',
-        BedrockXSTSRelyingParty = 'https://multiplayer.minecraft.net/',
-        XboxXSTSRelyingParty = 'http://auth.xboxlive.com/'
-    }
-
+  export enum RelyingParty {
+    PCXSTSRelyingParty = 'rp://api.minecraftservices.com/',
+    BedrockXSTSRelyingParty = 'https://multiplayer.minecraft.net/',
+    XboxXSTSRelyingParty = 'http://auth.xboxlive.com/'
+  }
 }
