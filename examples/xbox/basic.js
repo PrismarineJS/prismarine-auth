10477dc77b6fe9f433e4be65d0fb3963e33a4114
extremeheat <extreme@protonmail.ch>
2024-05-22 23:36:14 -0400

    add a lint error

--- examples/xbox/basic.js
+++ examples/xbox/basic.js
@@ -1,4 +1,4 @@
 const { Authflow } = require('prismarine-auth')
 // No parameters needed - will login as Minecraft by default unless a custom Azure client ID is passed (see ./azure.js)
-const flow = new Authflow()
+const flow = new Authflow() 
 module.exports = flow.getXboxToken().then(console.log)
