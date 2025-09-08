fcb0e209e915e193dd05a1636ec0bf9526e8e2d7
LucienHH <lucienharryholloway@hotmail.co.uk>
2025-09-08 10:13:51 +0100

    Update tests

--- test/password.test.js
+++ test/password.test.js
@@ -9,6 +9,11 @@ const { Authflow, Titles } = require('prismarine-auth')
 describe('password authentication', async () => {
   it('should fail if not given a valid password', async () => {
     const flow = new Authflow('this.is.not@valid.email.lol', './test', { password: 'sdfasdfas', flow: 'live', authTitle: Titles.MinecraftJava })
-    await expect(flow.getXboxToken()).to.eventually.be.rejectedWith('Invalid credentials')
+    try {
+      await flow.getXboxToken()
+      expect.fail('Expected authentication to fail, but it succeeded')
+    } catch (error) {
+      expect(error.data.attributes).to.have.property('code', 'INVALID_CREDENTIALS_OR_2FA_ENABLED')
+    }
   })
 })
